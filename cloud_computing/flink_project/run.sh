c50a35830c5035a0df700cd08c904def6fe7494b
Filip Finfando <filip.finfando@gmail.com>
2018-10-06 17:30:27 +0200

    removed maven, now only flink is provided

--- cloud_computing/flink_project/run.sh
+++ cloud_computing/flink_project/run.sh
@@ -1,10 +1,6 @@
-cp -r /home/programs /home/maven_project/eit_artifact/src/main/java/eit_group
-cd /home/maven_project/eit_artifact
-mvn clean install -Pbuild-jar
-/home/flink_app/flink-1.3.3/bin/start-cluster.sh
-sleep 1
-# /home/flink_app/flink-1.3.3/bin/flink run -c eit_group.WordCount /home/maven_project/eit_artifact/target/eit_artifact-1.0-SNAPSHOT.jar
-/home/flink_app/flink-1.3.3/bin/flink run -c eit_group.MapFilter /home/maven_project/eit_artifact/target/eit_artifact-1.0-SNAPSHOT.jar \
-    /home/data-in/test_data.csv \
-    /home/data-out/test_out.txt
-# sleep 60
+source ./var.env
+execute_this="//opt//flink//bin//flink run -c $GROUP_ID.$1 //home//jar//$ARTIFACT_JAR_NAME.jar"
+shift
+execute_this="$execute_this $@"
+echo $execute_this
+docker container exec flinky $execute_this
